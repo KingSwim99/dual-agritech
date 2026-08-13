10356de5612f6ed5be73fbd759e30c90fc4f8d51
hans-velecina <hans.velecina.099@gmail.com>
2026-08-13 20:29:16 +0800

    Added new images

--- app/controllers/site/gallery_controller.rb
+++ app/controllers/site/gallery_controller.rb
@@ -8,7 +8,12 @@ module Site
       21 => "Sunjin Deployment",
       22 => "Sunjin Deployment",
       23 => "Sealed the OJT partnership with Tony Magno - Father of Tunnel Ventilation Poultry Farming.",
-      24 => "BOT PPAFI meeting with FEU Cavite Officials @ Dagatan Training Site"
+      24 => "BOT PPAFI meeting with FEU Cavite Officials @ Dagatan Training Site",
+      25 => "With Cafe de Lipa President & Owner Omar Mercado & PPAFI Chairman Sir Pol Titular",
+      26 => "PPAFI & Cafe de Lipa MOA Signing",
+      27 => "PPAFI & Cafe de Lipa MOA Signing",
+      28 => "PPAFI & Cafe de Lipa MOA Signing",
+      31 => "Coordination meeting with Cuenca LGU"
     }.freeze
 
     def index
@@ -19,11 +24,11 @@ module Site
     private
 
     def load_ppafi_images
-      Dir.glob(Rails.root.join("app/assets/images/ppafi/gallery_*.jpg"))
-         .sort_by { |f| File.basename(f, ".jpg").split("_").last.to_i }
+      Dir.glob(Rails.root.join("app/assets/images/ppafi/gallery_*.{jpg,jpeg}"))
+         .sort_by { |f| File.basename(f).split("_").last.to_i }
          .reverse
          .map do |f|
-           n = File.basename(f, ".jpg").split("_").last.to_i
+           n = File.basename(f).split("_").last.to_i
            { path: "ppafi/#{File.basename(f)}", description: PPAFI_DESCRIPTIONS[n] }
          end
     end
